e4b415bc9e15db37236eb14ba01a872e7a40aaba
Hin Chi WONG <phantomcase@gmail.com>
2021-12-19 09:51:27 +0000

    Exercise 2

---
+++ src/com/techreturner/introtooop/Main.java
@@ -0,0 +1,9 @@
+package com.techreturner.introtooop;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird myBird = new Bird("Tweety", "Yellow");
+        myBird.speak();
+        myBird.fly();
+    }
+}
